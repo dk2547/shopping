889a398e818c27411a030c40039ff4df92cc8b5b
Sameer Ranjan <dk2547@gmail.com>
2022-03-01 21:33:24 +0530

    new env added

--- src/main/java/com/sam/shopping/controller/ResourceController.java
+++ src/main/java/com/sam/shopping/controller/ResourceController.java
@@ -1,12 +1,15 @@
 package com.sam.shopping.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
+    @Value("${env}")
+    String env;
     @GetMapping
-    public String getResponce(){
-        return "hello heroku - 1";
+    public String ping(){
+        return "Application is up in "+env;
     }
 }
